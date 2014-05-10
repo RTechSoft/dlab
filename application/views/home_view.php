21a47fbdf582953f262bffaed882d5aab4c68d58
dafrancisco <dens1515@gmail.com>
2014-05-10 18:48:55 +0800

    ui stuff

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -32,7 +32,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.php">Start Bootstrap</a>
+                <a class="navbar-brand" href="index.php">iRescue</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
@@ -60,14 +60,33 @@
     </nav>
 
     <div class="container">
-
-        <div class="row">
-            <div class="col-lg-12">
-                <h1>A better Bootstrap starter template.</h1>
-                <p>Complete with pre-defined file paths that you won't have to change!</p>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                  <ul class="nav navbar-nav">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Resources <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Put it here</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Public <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Private</a></li>
+                            <li><a href="#">All</a></li>
+                        </ul>
+                    </li>
+                  </ul>
+                  <form class="navbar-form navbar-left">
+                    <div class="form-group">
+                      <input type="text" class="form-control" placeholder="Quantity">
+                    </div>
+                    <button type="submit" class="btn btn-default">Go</button>
+                  </form>
+                </div><!-- /.navbar-collapse -->
             </div>
-        </div>
-
+        </nav>
     </div>
     <!-- /.container -->
 
